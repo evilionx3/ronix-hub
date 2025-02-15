0cc8d63102162058cb795ed7031134725d3915c1
evilionx3 <wylan.yandell@gmail.com>
2025-02-14 16:11:39 -0900

    Update real.lua

--- real.lua
+++ real.lua
@@ -1,2 +1,2 @@
-queueteleport("loadstring(game:HttpGet("https://raw.githubusercontent.com/evilionx3/ronix-hub/refs/heads/main/real.lua"))()")
+queue_on_teleport("loadstring(game:HttpGet("https://raw.githubusercontent.com/evilionx3/ronix-hub/refs/heads/main/real.lua"))()")
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/47fc2ea07fef3d78fb68ca87ab2b7503.lua"))()
